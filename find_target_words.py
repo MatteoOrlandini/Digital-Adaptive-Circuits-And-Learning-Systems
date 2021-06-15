fbd0595ed6bc9747a1262b4ec5bf1fb50102484e
MatteoOrlandini <48382335+MatteoOrlandini@users.noreply.github.com>
2021-06-15 12:58:44 +0200

    Update find_target_words.py

--- find_target_words.py
+++ find_target_words.py
@@ -4,29 +4,42 @@ import os
 import collections
 
 words = []
-target_words = [{
-	'word': [],
-	'frequency': 0,
-	'start' : []
-}]
+target_words = []
+# target_words = [{
+	# 'word': [],
+	# 'frequency': 0,
+	# 'start' : []
+# }]
 
 folder = []
-source_path = "../Progetti Github/Digital-Adaptive-Circuits-And-Learning-Systems/Dataset/English spoken wikipedia/english/Asperger_syndrome/"
+source_path = "./Dataset/English spoken wikipedia/english/Asperger_syndrome/"
 filename = "aligned.swc"
 
 #for path in os.scandir(source_path):
 if (os.path.exists(source_path + "/" + filename)):
-    tree = ET.parse(source_path + "/" + filename)
+	tree = ET.parse(source_path + "/" + filename)
     # getroot returns the root element for this tree
-    root = tree.getroot()
+	root = tree.getroot()
     # root.iter creates a tree iterator with the current element as the root. The iterator iterates over this element and all elements below it, in document (depth first) order.
-    for token_normalization in root.iter(tag = 'n'):
-        if 'start' in token_normalization.keys():
-            words.append(token_normalization.attrib['pronunciation'].lower())
-    unique = collections.Counter(words)
-    print(unique)
-    for word in unique:
-        if word.values
+	for token_normalization in root.iter(tag = 'n'):
+		if 'start' in token_normalization.keys():
+			# add every work with "start" key
+			words.append(token_normalization.attrib['pronunciation'].lower())
+	# collections.Counter stores elements as dictionary keys, and their counts are stored as dictionary values.
+	unique_words = collections.Counter(words)
+	print("unique_words", unique_words)
+	print("unique_words length:", len(unique_words))
+	for key in unique_words.keys():
+		# we only consider words that occur at least 10 times in the recording
+		if (unique_words[key] > 9):
+			target_words.append({'word':key, 'frequency': unique_words[key]})
+			
+	print("target_words:", target_words)
+	print("target_words length:", len(target_words))
+	
+	
+    #for word in unique:
+        #if word.values
     #unique.keys
             # if not any(item['word'] == token_normalization.attrib['pronunciation'].lower() for item in target_words):
                 # dict = {'word' : token_normalization.attrib['pronunciation'], \
