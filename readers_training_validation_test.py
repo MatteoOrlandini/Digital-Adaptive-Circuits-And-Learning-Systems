f9b090672d6448eaf03e0f5dedc7c4bf6bfcc6ae
MatteoOrlandini <48382335+MatteoOrlandini@users.noreply.github.com>
2021-06-18 14:33:44 +0200

    update readers_training_validation_test.py

--- readers_training_validation_test.py
+++ readers_training_validation_test.py
@@ -1,72 +1,60 @@
 import json
 import random
 
-f = open("readers_words.json", "r")
-readers = json.load(f)
-f.close()
+def write_json_file(filename, list_of_dict):
+    f = open(filename, "w")
+    f.write(json.dumps(list_of_dict, indent = 4))
+    f.close
 
-valid_readers = []
-training_readers = []
-test_and_validation_readers = []
-test_readers = []
-validation_readers = []
+def find_valid_readers(C, K):
+    f = open("readers_words.json", "r")
+    readers = json.load(f)
+    f.close()
 
-C = 2 # classes
-K = 10 # instances per class
-Q = 16 # query set
+    valid_readers = []
 
-for reader in readers:    
-    valid_words = []
-    number_of_words_per_reader = 0
-    is_valid_reader = False
-    number_of_valid_words = 0
+    Q = 16 # query set
 
-    for word in reader['words']:
-        start = []
-        end = []
-        folders = []
+    for reader in readers:    
+        valid_words = []
+        number_of_words_per_reader = 0
+        is_valid_reader = False
+        number_of_valid_words = 0
 
-        for item in word['folders']:
-            start += item['start']
-            end += item['end']
-            folders += [item['folder']]*len(item['start'])
+        for word in reader['words']:
+            start = []
+            end = []
+            folders = []
 
-        new_word = {'word'  : word['word'], \
-                    'start' : start, \
-                    'end'   : end, \
-                    'folder': folders}
+            for item in word['folders']:
+                start += item['start']
+                end += item['end']
+                folders += [item['folder']]*len(item['start'])
 
-        if (len(new_word['start']) >= K + Q):
-            number_of_valid_words += 1
-            valid_words.append(new_word)
+            new_word = {'word'  : word['word'], \
+                        'start' : start, \
+                        'end'   : end, \
+                        'folder': folders}
 
-    if (number_of_valid_words >= C):
-        is_valid_reader = True
+            if (len(new_word['start']) >= K + Q):
+                number_of_valid_words += 1
+                valid_words.append(new_word)
 
-    if (is_valid_reader):
-        valid_readers.append({'reader_name' : reader['reader_name'],\
-                                 'words': valid_words})
+        if (number_of_valid_words >= C):
+            is_valid_reader = True
 
-if (len(valid_readers) >= 183):
-    number_of_training_readers = 138
-    number_of_test_readers = 30
-    number_of_validation_readers = 15
+        if (is_valid_reader):
+            valid_readers.append({'reader_name' : reader['reader_name'],\
+                                    'words': valid_words})
 
-    # taking only 183 valid readers
-    valid_readers = random.sample(valid_readers, 183)
-    training_readers = random.sample(valid_readers, number_of_training_readers)
-    for item in valid_readers:
-        if not any(item['reader_name'] == training_reader['reader_name'] for training_reader in training_readers):
-            test_and_validation_readers.append(item)
+    return valid_readers
 
-    test_readers = test_and_validation_readers[0:number_of_test_readers]
+def create_training_validation_test_readers(valid_readers, number_of_training_readers, number_of_test_readers, number_of_validation_readers):
+    training_readers = []
+    test_and_validation_readers = []
+    test_readers = []
+    validation_readers = []
 
-    validation_readers = test_and_validation_readers[number_of_test_readers:number_of_test_readers+number_of_validation_readers]
-
-else:
-    number_of_training_readers = int(138/183*len(valid_readers))
-    number_of_test_readers = int(30/183*len(valid_readers))
-    number_of_validation_readers = int(15/183*len(valid_readers))
     training_readers = random.sample(valid_readers, number_of_training_readers)
 
     for item in valid_readers:
@@ -77,14 +65,24 @@ else:
 
     validation_readers = test_and_validation_readers[number_of_test_readers:number_of_test_readers+number_of_validation_readers]
 
-f = open("training_readers.json", "w")
-f.write(json.dumps(training_readers, indent = 0))
-f.close
+    write_json_file("training_readers.json", training_readers)
+    write_json_file("test_readers.json", test_readers)
+    write_json_file("validation_readers.json", validation_readers)
 
-f = open("test_readers.json", "w")
-f.write(json.dumps(test_readers, indent = 0))
-f.close
 
-f = open("validation_readers.json", "w")
-f.write(json.dumps(validation_readers, indent = 0))
-f.close
+if __name__ == "__main__":
+    C = 2 # classes
+    K = 10 # instances per class
+    valid_readers = find_valid_readers(C, K)
+
+    if (len(valid_readers) >= 183):
+        number_of_training_readers = 138
+        number_of_test_readers = 30
+        number_of_validation_readers = 15
+
+    else:
+        number_of_training_readers = int(138/183*len(valid_readers))
+        number_of_test_readers = int(30/183*len(valid_readers))
+        number_of_validation_readers = int(15/183*len(valid_readers))
+
+    create_training_validation_test_readers(valid_readers, number_of_training_readers, number_of_test_readers, number_of_validation_readers)
