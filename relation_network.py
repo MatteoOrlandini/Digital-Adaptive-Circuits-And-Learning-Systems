5287ccf6605a42a65fc6771eccecf3107c206238
MatteoOrlandini <48382335+MatteoOrlandini@users.noreply.github.com>
2021-07-13 12:09:12 +0200

    update relation_training.py

--- relation_network.py
+++ relation_network.py
@@ -4,7 +4,12 @@ import math
 import torch.nn.functional as F
 
 class CNNEncoder(nn.Module):
-    """docstring for ClassName"""
+    """
+    CNNEncoder is the embedding module. The architecture consists of 4 convolutional block contains a 
+    64-filter 3 X 3 convolution, a batch normalisation and a ReLU nonlinearity layer respectively. 
+    The first 3 blocks also contain a 2 X 2 max-pooling layer while the last two do not. We do so 
+    because we need the output feature maps for further convolutional layers in the relation module
+    """
     def __init__(self):
         super(CNNEncoder, self).__init__()
         self.layer1 = nn.Sequential(
@@ -37,11 +42,18 @@ class CNNEncoder(nn.Module):
         return out # 64
 
 class RelationNetwork(nn.Module):
-    """docstring for RelationNetwork"""
+    """
+    The RelationNetwork is the relation module. It consists of two convolutional blocks and
+    two fully-connected layers. Each of convolutional block is a 3 X 3 convolution with 64 filters 
+    followed by batch normalisation, ReLU non-linearity and 2 X 2 max-pooling.
+    The two fully-connected layers are 8 and 1 dimensional, respectively. All fully-connected layers are
+    ReLU except the output layer is Sigmoid in order to generate relation scores in a reasonable range 
+    for all versions of our network architecture.
+    """
     def __init__(self,input_size,hidden_size):
         super(RelationNetwork, self).__init__()
         self.layer1 = nn.Sequential(
-                        nn.Conv2d(input_size,64,kernel_size=3,padding=1),
+                        nn.Conv2d(128,64,kernel_size=3,padding=1),
                         nn.BatchNorm2d(64, momentum=1, affine=True),
                         nn.ReLU(),
                         nn.MaxPool2d(2))
@@ -50,21 +62,16 @@ class RelationNetwork(nn.Module):
                         nn.BatchNorm2d(64, momentum=1, affine=True),
                         nn.ReLU(),
                         nn.MaxPool2d(2))
-        self.fc1 = nn.Linear(input_size,hidden_size)
-        self.fc2 = nn.Linear(hidden_size,1)
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, 1)
 
     def forward(self,x):
-        print(x.shape)
-        out = self.layer1(x)
-        print(out.shape)
-        out = self.layer2(out)
-        print(out.shape)
-        out = out.view(out.size(0),-1)
-        print(out.shape)
-        out = F.relu(self.fc1(out))
-        print(out.shape)
-        out = F.sigmoid(self.fc2(out))
-        print(out.shape)
+        out = self.layer1(x)    #  (CLASS_NUM * BATCH_NUM_PER_CLASS * 5) X FEATURE_DIM X 2 X 2
+        out = self.layer2(out)  #  (CLASS_NUM * BATCH_NUM_PER_CLASS * 5) X FEATURE_DIM X 1 X 1
+        out = out.view(out.size(0),-1)   #  (CLASS_NUM * BATCH_NUM_PER_CLASS * 5) X FEATURE_DIM
+        out = F.relu(self.fc1(out))      #  (CLASS_NUM * BATCH_NUM_PER_CLASS * 5) X RELATION_DIM
+        #out = F.sigmoid(self.fc2(out)) # deprecated
+        out = torch.sigmoid(self.fc2(out))  #  (CLASS_NUM * BATCH_NUM_PER_CLASS * 5) X 1
         return out
 
 def weights_init(m):
--- relation_training.py
+++ relation_training.py
@@ -10,10 +10,12 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from torch.optim.lr_scheduler import StepLR
+import torch.nn.functional as F
 import numpy as np
 import argparse
 from relation_network import *
 from utils import *
+from tqdm import tqdm, trange
 
 """
 parser = argparse.ArgumentParser(description="One Shot Visual Recognition")
@@ -32,7 +34,7 @@ args = parser.parse_args()
 
 
 # Hyper Parameters
-FEATURE_DIM = 192
+FEATURE_DIM = 64
 RELATION_DIM = 8
 CLASS_NUM = 5
 SAMPLE_NUM_PER_CLASS = 5
@@ -40,7 +42,7 @@ BATCH_NUM_PER_CLASS = 16
 EPISODE = 60000
 TEST_EPISODE = 1000
 LEARNING_RATE = 0.001
-GPU = 0 # mettere a 1 in colab
+GPU = 0
 HIDDEN_UNIT = 10
 
 def main():
@@ -54,7 +56,7 @@ def main():
     print("init neural networks")
 
     feature_encoder = CNNEncoder()
-    relation_network = RelationNetwork(FEATURE_DIM,RELATION_DIM)
+    relation_network = RelationNetwork(FEATURE_DIM, RELATION_DIM)
 
     feature_encoder.apply(weights_init)
     relation_network.apply(weights_init)
@@ -63,10 +65,10 @@ def main():
         feature_encoder.to(device='cuda')
         relation_network.to(device='cuda')
 
-    feature_encoder_optim = torch.optim.Adam(feature_encoder.parameters(),lr=LEARNING_RATE)
-    feature_encoder_scheduler = StepLR(feature_encoder_optim,step_size=100000,gamma=0.5)
-    relation_network_optim = torch.optim.Adam(relation_network.parameters(),lr=LEARNING_RATE)
-    relation_network_scheduler = StepLR(relation_network_optim,step_size=100000,gamma=0.5)
+    feature_encoder_optim = torch.optim.Adam(feature_encoder.parameters(), lr = LEARNING_RATE)
+    #feature_encoder_scheduler = StepLR(feature_encoder_optim,step_size=100000,gamma=0.5)
+    relation_network_optim = torch.optim.Adam(relation_network.parameters(), lr = LEARNING_RATE)
+    #relation_network_scheduler = StepLR(relation_network_optim,step_size=100000,gamma=0.5)
 
     '''
     if os.path.exists(str("./models/omniglot_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")):
@@ -82,10 +84,10 @@ def main():
 
     last_accuracy = 0.0
 
-    for episode in range(EPISODE):
+    for episode in trange(EPISODE, desc = "episode", position = 0, leave = True):
 
-        feature_encoder_scheduler.step(episode)
-        relation_network_scheduler.step(episode)
+        #feature_encoder_scheduler.step(episode)
+        #relation_network_scheduler.step(episode)
 
         # init dataset
         # sample_dataloader is to obtain previous samples for compare
@@ -97,9 +99,15 @@ def main():
         batch_dataloader = tg.get_data_loader(task,num_per_class=BATCH_NUM_PER_CLASS,split="test",shuffle=True,rotation=degrees)
         '''
 
-        batches, samples = batch_sample(training_readers, CLASS_NUM, SAMPLE_NUM_PER_CLASS, BATCH_NUM_PER_CLASS)
-        samples = samples.view(CLASS_NUM * SAMPLE_NUM_PER_CLASS, 1, *samples.size()[2:])  # (C X K) X 1 X 128 X 51
-        batches = batches.view(CLASS_NUM * BATCH_NUM_PER_CLASS, 1, *batches.size()[2:])   # (C X Q) X 1 X 128 X 51
+        batches, samples = batch_sample(training_readers, CLASS_NUM, SAMPLE_NUM_PER_CLASS, BATCH_NUM_PER_CLASS) # samples: C X K X 128 X 51,  batches: C X Q X 128 X 51
+        #print(batches.shape)
+        # resize the images from * X 128 X 51 to * X 51 X 51 to get square images
+        # interpolate down samples the input to the given size
+        samples = F.interpolate(samples, size = 51)
+        batches = F.interpolate(batches, size = 51)
+        #print(batches.shape)
+        samples = samples.view(CLASS_NUM * SAMPLE_NUM_PER_CLASS, 1, *samples.size()[2:])  # (C X K) X 1 X 51 X 51
+        batches = batches.view(CLASS_NUM * BATCH_NUM_PER_CLASS, 1, *batches.size()[2:])   # (C X Q) X 1 X 51 X 51
 
         if torch.cuda.is_available():
             samples = samples.to(device='cuda')
@@ -108,33 +116,32 @@ def main():
         # sample datas
         #samples,sample_labels = sample_dataloader.__iter__().next()
         #batches,batch_labels = batch_dataloader.__iter__().next()
-        batch_labels = torch.arange(0, CLASS_NUM).view(CLASS_NUM, 1, 1).expand(CLASS_NUM, BATCH_NUM_PER_CLASS, 1).long()
+        #batch_labels = torch.arange(0, CLASS_NUM).view(CLASS_NUM, 1, 1).expand(CLASS_NUM, BATCH_NUM_PER_CLASS, 1).long()
+        batch_labels = torch.arange(0, CLASS_NUM)   # [0, 1, 2, ..., CLASS_NUM]
         # calculate features
-        sample_features = feature_encoder(Variable(samples)) # 5x64*5*5
-        print("sample_features.shape:",sample_features.shape)
-        sample_features = sample_features.view(CLASS_NUM, SAMPLE_NUM_PER_CLASS, FEATURE_DIM, 5, 5) #-1
+        sample_features = feature_encoder(Variable(samples)) # (CLASS_NUM * SAMPLE_NUM_PER_CLASS) X FEATURE_DIM X 5 X 5
+        #print("sample_features.shape:",sample_features.shape)
+        sample_features = sample_features.view(CLASS_NUM, SAMPLE_NUM_PER_CLASS, FEATURE_DIM, 5, 5) # CLASS_NUM X SAMPLE_NUM_PER_CLASS X FEATURE_DIM X 5 X 5
         sample_features = torch.sum(sample_features,1).squeeze(1)
-        print("sample_features.shape after view and sum:", sample_features.shape)
+        #print("sample_features.shape after view and sum:", sample_features.shape)   # CLASS_NUM X FEATURE_DIM X 5 X 5
 
-        batch_features = feature_encoder(Variable(batches)) # 20x64*5*5
-        print("batch_features.shape:", batch_features.shape)
-        #batch_features = batch_features.view(SAMPLE_NUM_PER_CLASS,FEATURE_DIM,5,5) #aggiunta!!!
-        print("batch_features.shape after view:", batch_features.shape)
+        batch_features = feature_encoder(Variable(batches)) # (CLASS_NUM * BATCH_NUM_PER_CLASS) X FEATURE_DIM X 5 X 5
+        #print("batch_features.shape:", batch_features.shape)
 
         # calculate relations
         # each batch sample link to every samples to calculate relations
         # to form a 100x128 matrix for relation network
-        sample_features_ext = sample_features.unsqueeze(0).repeat(BATCH_NUM_PER_CLASS*CLASS_NUM,1,1,1,1)
-        print("sample_features_ext.shape after repeat:", sample_features_ext.shape)
-        batch_features_ext = batch_features.unsqueeze(0).repeat(CLASS_NUM,1,1,1,1)
-        print("batch_features_ext.shape after repeat:", batch_features_ext.shape)
-        batch_features_ext = torch.transpose(batch_features_ext,0,1)
-        print("batch_features_ext.shape after transport:", batch_features_ext.shape)
-        #batch_features_ext = batch_features_ext.view(BATCH_NUM_PER_CLASS*CLASS_NUM,CLASS_NUM,FEATURE_DIM,5,5)
-        #print("batch_ext_f size:",batch_features_ext.shape)
+        sample_features_ext = sample_features.unsqueeze(0).repeat(CLASS_NUM * BATCH_NUM_PER_CLASS, 1, 1, 1, 1) # (CLASS_NUM * BATCH_NUM_PER_CLASS) X 5 X FEATURE_DIM X 5 X 5
+        #print("sample_features_ext.shape after repeat:", sample_features_ext.shape)
+        batch_features_ext = batch_features.unsqueeze(0).repeat(CLASS_NUM, 1, 1, 1, 1)  # 5 X (CLASS_NUM * BATCH_NUM_PER_CLASS) X FEATURE_DIM X 5 X 5
+        #print("batch_features_ext.shape after repeat:", batch_features_ext.shape)
+        batch_features_ext = torch.transpose(batch_features_ext, 0, 1)  #  (CLASS_NUM * BATCH_NUM_PER_CLASS) X 5 X FEATURE_DIM X 5 X 5
+        #print("batch_features_ext.shape after transpose:", batch_features_ext.shape)
 
-        relation_pairs = torch.cat((sample_features_ext,batch_features_ext),2).view(-1,FEATURE_DIM*2,5,5)
-        relations = relation_network(relation_pairs).view(-1,CLASS_NUM)
+        relation_pairs = torch.cat((sample_features_ext,batch_features_ext), 2).view(-1, FEATURE_DIM*2, 5, 5)  #  (CLASS_NUM * BATCH_NUM_PER_CLASS * 5) X (FEATURE_DIM * 2) X 5 X 5
+        #print("relation_pairs.shape :", relation_pairs.shape)
+        relations = relation_network(relation_pairs).view(-1,CLASS_NUM) #  (CLASS_NUM * BATCH_NUM_PER_CLASS) X CLASS_NUM
+        #print("relations.shape :", relations.shape)
 
         mse = nn.MSELoss()
         one_hot_labels = Variable(torch.zeros(BATCH_NUM_PER_CLASS*CLASS_NUM, CLASS_NUM).scatter_(1, batch_labels.view(-1,1), 1))
@@ -152,8 +159,8 @@ def main():
 
         loss.backward()
 
-        torch.nn.utils.clip_grad_norm(feature_encoder.parameters(),0.5)
-        torch.nn.utils.clip_grad_norm(relation_network.parameters(),0.5)
+        torch.nn.utils.clip_grad_norm_(feature_encoder.parameters(), 0.5)
+        torch.nn.utils.clip_grad_norm_(relation_network.parameters(), 0.5)
 
         feature_encoder_optim.step()
         relation_network_optim.step()
