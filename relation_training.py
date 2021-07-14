ccfd844b065264a1c0706c1732c1355f7ed9be25
MatteoOrlandini <48382335+MatteoOrlandini@users.noreply.github.com>
2021-07-14 12:22:08 +0200

    Update relation_training.py

--- relation_training.py
+++ relation_training.py
@@ -72,7 +72,9 @@ def main():
     
     train_loss = []
 
-    for episode in trange(EPISODE, desc = "episode", position = 0, leave = True):
+    last_accuracy = 0.0
+
+    for episode in trange(EPISODE, desc = "training episode", position = 0, leave = True):
 
         # sample datas
         batches, samples = batch_sample(training_readers, CLASS_NUM, SAMPLE_NUM_PER_CLASS, BATCH_NUM_PER_CLASS) # samples: C X K X 128 X 51,  batches: C X Q X 128 X 51
@@ -134,92 +136,87 @@ def main():
 
         train_loss.append(loss.item())
 
-    print("Training loss: {}".format(train_loss))
+        # validation
 
-    avg_loss_tr = np.mean(train_loss)
-    print('Average train loss: {}'.format(avg_loss_tr))
+        if (episode+1)%5000 == 0:
+            total_rewards = 0
+            for i in trange(TEST_EPISODE, desc = "validation episode", position = 1, leave = True):
 
-    torch.save({
-                'epoch': 60000,
-                'feature_encoder_state_dict': feature_encoder.state_dict(),
-                'relation_network_state_dict' : relation_network.state_dict(),
-                'feature_encoder_optim_state_dict': feature_encoder_optim.state_dict(),
-                'relation_network_optim_state_dict': relation_network_optim.state_dict(),
-                'loss': train_loss,
-                'avg_loss_tr' : avg_loss_tr,
-                }, "Models/Relation/relation_model_C{}_K{}_60000epi.pt".format(CLASS_NUM, SAMPLE_NUM_PER_CLASS))
+                # sample datas
+                batches, samples = batch_sample(validation_readers, CLASS_NUM, SAMPLE_NUM_PER_CLASS, BATCH_NUM_PER_CLASS) # samples: C X K X 128 X 51,  batches: C X Q X 128 X 51
+                
+                samples = samples.view(CLASS_NUM * SAMPLE_NUM_PER_CLASS, 1, *samples.size()[2:])  # (C X K) X 1 X 51 X 51
+                batches = batches.view(CLASS_NUM * BATCH_NUM_PER_CLASS, 1, *batches.size()[2:])   # (C X Q) X 1 X 51 X 51
 
-    print ("Validation...")
+                if torch.cuda.is_available():
+                    samples = samples.to(device='cuda')
+                    batches = batches.to(device='cuda')
 
-    validation_loss = []
-    for episode in trange(EPISODE, desc = "episode", position = 0, leave = True):
+                # calculate features
+                sample_features = feature_encoder(Variable(samples)) # (CLASS_NUM * SAMPLE_NUM_PER_CLASS) X FEATURE_DIM X 5 X 5
 
-        # sample datas
-        batches, samples = batch_sample(validation_readers, CLASS_NUM, SAMPLE_NUM_PER_CLASS, BATCH_NUM_PER_CLASS) # samples: C X K X 128 X 51,  batches: C X Q X 128 X 51
-        
-        samples = samples.view(CLASS_NUM * SAMPLE_NUM_PER_CLASS, 1, *samples.size()[2:])  # (C X K) X 1 X 51 X 51
-        batches = batches.view(CLASS_NUM * BATCH_NUM_PER_CLASS, 1, *batches.size()[2:])   # (C X Q) X 1 X 51 X 51
+                # resize the images from 15 X 5 to 5 X 5 to get square images
+                # interpolate down samples the input to the given size
+                sample_features = F.interpolate(sample_features, size = 5)
+                sample_features = sample_features.view(CLASS_NUM, SAMPLE_NUM_PER_CLASS, FEATURE_DIM, 5, 5) # CLASS_NUM X SAMPLE_NUM_PER_CLASS X FEATURE_DIM X 5 X 5
+                sample_features = torch.sum(sample_features,1).squeeze(1)   # CLASS_NUM X FEATURE_DIM X 5 X 5
 
-        if torch.cuda.is_available():
-            samples = samples.to(device='cuda')
-            batches = batches.to(device='cuda')
+                batch_features = feature_encoder(Variable(batches)) # (CLASS_NUM * BATCH_NUM_PER_CLASS) X FEATURE_DIM X 5 X 5
+                batch_features = F.interpolate(batch_features, size = 5)
 
-        # calculate features
-        sample_features = feature_encoder(Variable(samples)) # (CLASS_NUM * SAMPLE_NUM_PER_CLASS) X FEATURE_DIM X 5 X 5
+                # calculate relations
+                # each batch sample link to every samples to calculate relations
+                sample_features_ext = sample_features.unsqueeze(0).repeat(CLASS_NUM * BATCH_NUM_PER_CLASS, 1, 1, 1, 1) # (CLASS_NUM * BATCH_NUM_PER_CLASS) X 5 X FEATURE_DIM X 5 X 5
+                batch_features_ext = batch_features.unsqueeze(0).repeat(CLASS_NUM, 1, 1, 1, 1)  # 5 X (CLASS_NUM * BATCH_NUM_PER_CLASS) X FEATURE_DIM X 5 X 5
+                batch_features_ext = torch.transpose(batch_features_ext, 0, 1)  #  (CLASS_NUM * BATCH_NUM_PER_CLASS) X 5 X FEATURE_DIM X 5 X 5
 
-        # resize the images from 15 X 5 to 5 X 5 to get square images
-        # interpolate down samples the input to the given size
-        sample_features = F.interpolate(sample_features, size = 5)
-        sample_features = sample_features.view(CLASS_NUM, SAMPLE_NUM_PER_CLASS, FEATURE_DIM, 5, 5) # CLASS_NUM X SAMPLE_NUM_PER_CLASS X FEATURE_DIM X 5 X 5
-        sample_features = torch.sum(sample_features,1).squeeze(1)   # CLASS_NUM X FEATURE_DIM X 5 X 5
+                relation_pairs = torch.cat((sample_features_ext,batch_features_ext), 2).view(-1, FEATURE_DIM*2, 5, 5)  #  (CLASS_NUM * BATCH_NUM_PER_CLASS * 5) X (FEATURE_DIM * 2) X 5 X 5
+                relations = relation_network(relation_pairs).view(-1,CLASS_NUM) #  (CLASS_NUM * BATCH_NUM_PER_CLASS) X CLASS_NUM
 
-        batch_features = feature_encoder(Variable(batches)) # (CLASS_NUM * BATCH_NUM_PER_CLASS) X FEATURE_DIM X 5 X 5
-        batch_features = F.interpolate(batch_features, size = 5)
+                _,predict_labels = torch.max(relations.data,1)
 
-        # calculate relations
-        # each batch sample link to every samples to calculate relations
-        sample_features_ext = sample_features.unsqueeze(0).repeat(CLASS_NUM * BATCH_NUM_PER_CLASS, 1, 1, 1, 1) # (CLASS_NUM * BATCH_NUM_PER_CLASS) X 5 X FEATURE_DIM X 5 X 5
-        batch_features_ext = batch_features.unsqueeze(0).repeat(CLASS_NUM, 1, 1, 1, 1)  # 5 X (CLASS_NUM * BATCH_NUM_PER_CLASS) X FEATURE_DIM X 5 X 5
-        batch_features_ext = torch.transpose(batch_features_ext, 0, 1)  #  (CLASS_NUM * BATCH_NUM_PER_CLASS) X 5 X FEATURE_DIM X 5 X 5
+                test_labels = torch.arange(CLASS_NUM).expand(BATCH_NUM_PER_CLASS,CLASS_NUM).transpose(1,0).reshape(-1)
 
-        relation_pairs = torch.cat((sample_features_ext,batch_features_ext), 2).view(-1, FEATURE_DIM*2, 5, 5)  #  (CLASS_NUM * BATCH_NUM_PER_CLASS * 5) X (FEATURE_DIM * 2) X 5 X 5
-        relations = relation_network(relation_pairs).view(-1,CLASS_NUM) #  (CLASS_NUM * BATCH_NUM_PER_CLASS) X CLASS_NUM
+                rewards = [1 if predict_labels[j]==test_labels[j] else 0 for j in range(CLASS_NUM*BATCH_NUM_PER_CLASS)]
 
-        mse = nn.MSELoss()
-        
-        one_hot_labels = Variable(torch.zeros(BATCH_NUM_PER_CLASS*CLASS_NUM, CLASS_NUM))
+                total_rewards += np.sum(rewards)
 
-        for i in range(CLASS_NUM):
-            for j in range(BATCH_NUM_PER_CLASS):
-                one_hot_labels[BATCH_NUM_PER_CLASS*i+j,i] = 1
+                '''
+                mse = nn.MSELoss()
+                
+                one_hot_labels = Variable(torch.zeros(BATCH_NUM_PER_CLASS*CLASS_NUM, CLASS_NUM))
 
-        if torch.cuda.is_available():
-            mse = mse.to(device='cuda')
-            one_hot_labels = one_hot_labels.to(device='cuda')
+                for i in range(CLASS_NUM):
+                    for j in range(BATCH_NUM_PER_CLASS):
+                        one_hot_labels[BATCH_NUM_PER_CLASS*i+j,i] = 1
 
-        loss = mse(relations,one_hot_labels)
+                if torch.cuda.is_available():
+                    mse = mse.to(device='cuda')
+                    one_hot_labels = one_hot_labels.to(device='cuda')
 
-        feature_encoder_optim.step()
-        relation_network_optim.step()
+                loss = mse(relations,one_hot_labels)
 
-        validation_loss.append(loss.item())
+                feature_encoder_optim.step()
+                relation_network_optim.step()
 
-    print("Validation loss: {}".format(validation_loss))
+                validation_loss.append(loss.item())
+                '''
+            test_accuracy = total_rewards/1.0/CLASS_NUM/SAMPLE_NUM_PER_CLASS/TEST_EPISODE
 
-    avg_loss_val = np.mean(validation_loss)
-    print('Average validation loss: {}'.format(avg_loss_val))
+            print("test accuracy:",test_accuracy)
 
-    torch.save({
-                'epoch': 60000,
-                'feature_encoder_state_dict': feature_encoder.state_dict(),
-                'relation_network_state_dict' : relation_network.state_dict(),
-                'feature_encoder_optim_state_dict': feature_encoder_optim.state_dict(),
-                'relation_network_optim_state_dict': relation_network_optim.state_dict(),
-                'training_loss': train_loss,
-                'validation_loss': validation_loss,
-                'avg_loss_tr' : avg_loss_tr,
-                'avg_loss_val' : avg_loss_val,
-                }, "Models/Relation/relation_model_validation_C{}_K{}_60000epi.pt".format(CLASS_NUM, SAMPLE_NUM_PER_CLASS))
-
+            if test_accuracy > last_accuracy:
+                torch.save({
+                            'epoch': episode,
+                            'feature_encoder_state_dict': feature_encoder.state_dict(),
+                            'relation_network_state_dict' : relation_network.state_dict(),
+                            'feature_encoder_optim_state_dict': feature_encoder_optim.state_dict(),
+                            'relation_network_optim_state_dict': relation_network_optim.state_dict(),
+                            'loss': train_loss,
+                            'avg_loss_tr' : np.mean(train_loss),
+                            'valid_accuracy' : test_accuracy,
+                            }, "Models/Relation/relation_model_C{}_K{}_60000epi.pt".format(CLASS_NUM, SAMPLE_NUM_PER_CLASS))
+                last_accuracy = test_accuracy
+            
 if __name__ == '__main__':
-    main()
+        main()
